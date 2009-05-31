4200e78ea1bca832ca2409cbe916acc585543590
Matt Liggett <mml@pobox.com>
2009-05-31 09:10:36 -0400

    Cleanup.

--- test.rb
+++ test.rb
@@ -9,11 +9,12 @@ require 'lang'
 parser = ObjLangParser.new
 programs = YAML::load_file 'programs.yml'
     
-programs.map(){|p| p.to_s}.each() {|p|
+puts 'parse'
+programs.map {|p| p.to_s}.each {|p|
   if parser.parse(p)
     print '.'
   else
     puts "failure: #{p}\n#{parser.failure_reason}"
   end
 }
-puts
+puts ''
