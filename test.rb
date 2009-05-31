bf6f65277ccd2966e6fa62f2b285c2df468bd4d7
Matt Liggett <mml@pobox.com>
2009-05-31 13:22:12 -0400

    Add a (heavily failing) deparsing test.

--- test.rb
+++ test.rb
@@ -6,16 +6,59 @@ require 'treetop'
 Treetop.load 'ol'
 require 'lang'
 
-parser = ObjLangParser.new
-programs = YAML::load_file 'programs.yml'
-programs.map! {|p| p.to_s }
-    
-puts 'parse'
-programs.each {|p|
-  if parser.parse(p)
-    print '.'
-  else
-    puts "failure: #{p}\n#{parser.failure_reason}"
-  end
-}
-puts ''
+def main
+  parser = ObjLangParser.new
+  programs = YAML::load_file 'programs.yml'
+  programs.map! {|p| p.to_s }
+      
+  puts 'parse'
+  programs.each {|p|
+    if parser.parse(p)
+      print '.'
+    else
+      puts "failure: #{p}\n#{parser.failure_reason}"
+    end
+  }
+
+  puts "\n\ndeparse"
+  programs.each {|p|
+    if ast = parser.parse(p)
+      a = run p
+      b = run ast.deparse
+
+      if a == b
+        print '.'
+      else
+        print 'x'
+      end
+    else
+      print 'S'
+    end
+  }
+
+  puts ''
+end
+
+def run p
+  out_r, out_w = IO.pipe
+  in_r, in_w = IO.pipe
+
+  fork {
+    in_w.close
+    out_r.close
+    $stderr.reopen out_w
+    $stdout.reopen out_w
+    $stdin.reopen in_r
+    exec 'ruby'
+  }
+  out_w.close
+  in_r.close
+  in_w.print p
+  in_w.close
+  result = out_r.read
+  out_r.close
+
+  result
+end
+
+main
