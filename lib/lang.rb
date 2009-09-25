09238c7e9602ae3a1987830cb1406b3d3ac7a42e
Matt Liggett <mml@pobox.com>
2009-09-24 21:53:24 -0400

    Factor out first_plus_rest (a common pattern), and also simplify Program.exprs().

--- lib/lang.rb
+++ lib/lang.rb
@@ -7,44 +7,43 @@ class Treetop::Runtime::SyntaxNode
 end
 
 module ObjLang
+  module Helpers
+    def first_plus_rest(l, extract)
+      if l.empty?
+        []
+      else
+        [l.first] + l.rest.elements.map(&extract)
+      end
+    end
+  end
+
   module Program
     def exprs
-      stmts.elements.map(&:expr) +
-        if expr.empty?
-          []
-        else
-          [expr]
-        end
+      stmts.elements.map(&:expr) + (expr.empty? ? [] : [expr])
     end
   end
 
   module MethDef
+    include Helpers
+
     def formal_ids
-      if formals.empty?
-        []
-      else
-        [formals.first] + formals.rest.elements.map(&:id)
-      end
+      first_plus_rest formals, :id
     end
   end
 
   module Message
+    include Helpers
+
     def param_exprs
-      if params.empty?
-        []
-      else
-        [params.first] + params.rest.elements.map(&:expr)
-      end
+      first_plus_rest params, :expr
     end
   end
 
   module Array
+    include Helpers
+
     def member_exprs
-      if members.empty?
-        []
-      else
-        [members.first] + members.rest.elements.map(&:expr)
-      end
+      first_plus_rest members, :expr
     end
   end
 
